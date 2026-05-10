7fb6f667452c1d01f687952049cb0c2b8061f002
sakharer198-create <sakharer198@gmail.com>
2026-05-10 18:34:45 +0530

    comm

--- src/main/java/com/sb/SbApplication.java
+++ src/main/java/com/sb/SbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SbApplication.class, args);
+    }
 
 }
